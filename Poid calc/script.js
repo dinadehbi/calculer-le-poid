ff76392102dba1df83b1ca11348a66543c6bbce1
dinadehbi <147596316+dinadehbi@users.noreply.github.com>
2024-01-10 16:58:35 +0100

    Add files via upload

---
+++ Poid calc/script.js
@@ -0,0 +1,36 @@
+let calculer = document.getElementById("btn");
+let check1 = document.getElementById("checkbox1");
+let check2 = document.getElementById("checkbox2");
+let check3 = document.getElementById("checkbox3");
+let input1 =  document.getElementById("inp1");
+let input2= document.getElementById("inp2");
+let demo = document.getElementById("demo");
+
+
+
+calculer.addEventListener("click", myFunction);
+function myFunction(){
+    if(check1.checked){
+        let Mince = (input1.value)-100+(input2.value/10)*0.9;
+        demo.innerHTML= Mince;
+        localStorage.setItem("value", Mince);
+
+    }else if (check2.checked) {
+        let Moyen = (input1.value)-100+(input2.value/10)*0.9*0.9;
+        demo.innerHTML= Moyen;
+        localStorage.setItem("value", Moyen);
+
+       // localStorage.setItem("value1", "Mince")
+
+
+
+    } else if(check3.checked){
+        let large = (input1.value)-100+(input2.value/10)*0.9*1.1;
+        demo.innerHTML= large;
+        localStorage.setItem("value", large);
+
+    }
+}
+if(localStorage.length >=0){
+    demo.innerHTML= localStorage.getItem("value")
+}
